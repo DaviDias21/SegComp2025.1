d49b5d7bc86b405a33f6b922a7a7d929752bac9d
Caio <caioiwakawa@gmail.com>
2025-05-23 12:53:07 -0300

    segunda parte do decifrador iniciada

--- Projeto-01_Cifra-de-Vigenere/decrypt.c
+++ Projeto-01_Cifra-de-Vigenere/decrypt.c
@@ -8,9 +8,13 @@ int keysize();
 int isRepeated();
 void displayFrequency();
 
+void keyletters();
+void printFrequency();
+
 int main(){
 
     char cipherTextMessage[MAXTXT];
+    float frequency_list[26] = {14.6, 1.0, 3.9, 5.0, 12.6, 1.0, 1.3, 1.3, 6.2, 0.4, 0.0, 2.8, 4.7, 5.0, 10.7, 2.5, 1.2, 6.5, 7.8, 4.3, 4.6, 1.7, 0.0, 0.2, 0.0, 0.5};
 
     FILE *inputFilePointer;
     char inputFileName[] = "input.txt";
@@ -25,6 +29,7 @@ int main(){
 
 
     int key_size = keysize(cipherTextMessage);
+    keyletters(cipherTextMessage, frequency_list, key_size);
     
     exit(0);
     return 0;
@@ -97,4 +102,53 @@ char x(int distance, int num){
 
 void displayFrequency(int cgroup[3], int d){
     printf("%11c%c%c%16d%7c%4c%4c%4c%4c%4c%4c%4c%5c%5c%5c%5c%5c%5c%5c%5c%5c%5c%5c\n", cgroup[0], cgroup[1], cgroup[2], d, x(d, 2), x(d, 3), x(d, 4), x(d, 5), x(d, 6), x(d, 7), x(d, 8), x(d, 9), x(d, 10), x(d, 11), x(d, 12), x(d, 13), x(d, 14), x(d, 15), x(d, 16), x(d, 17), x(d, 18), x(d, 19), x(d, 20));
+}
+
+void keyletters(char cText[MAXTXT], float fList[26], int keySize){
+
+    printf("LETTER FREQUENCY\n");
+    printFrequency(fList, 0);
+
+    int cur_shift = 0;
+    while(true){
+        char shift;
+        scanf("%c", &shift);
+        if(shift=='a'){
+            cur_shift++;
+        } else if(shift=='d'){
+            cur_shift--;
+        }
+        if(cur_shift==26){
+            cur_shift = 0;
+        } else if(cur_shift==-1){
+            cur_shift = 25;
+        }
+        printFrequency(fList, cur_shift);
+    }
+}
+
+void printFrequency(float frequency[26], int start){
+    for(int i=15; i>=0; i--){
+        for(int u=start; u<26; u++){
+            if (frequency[u] >= i){
+                printf(" --- ");
+            } else{
+                printf("     ");
+            }
+        }
+        for(int u=0; u<start; u++){
+            if (frequency[u] >= i){
+                printf(" --- ");
+            } else{
+                printf("     ");
+            }
+        }
+        printf("\n ");
+    }
+    for(int i=start; i<26; i++){
+        printf("%4.1f ", frequency[i]);
+    }
+    for(int i=0; i<start; i++){
+        printf("%4.1f ", frequency[i]);
+    }
 }
