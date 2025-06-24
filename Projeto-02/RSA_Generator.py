f850ef7ea437bcd8ae115b78859b91fdfc8e1a5c
Davi Dias <51387816+DaviDias21@users.noreply.github.com>
2025-06-24 02:39:15 -0300

    Update RSA_Generator.py

--- Projeto-02/RSA_Generator.py
+++ Projeto-02/RSA_Generator.py
@@ -1,6 +1,12 @@
 from sympy import randprime
+import hashlib
+import base64
+import time
+import sys
+import random
 
-LIMIT = 179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216
+#LIMIT = 179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216
+LIMIT = 128
 E = 65537
 
 class KeySet:
@@ -9,25 +15,43 @@ class KeySet:
         self.privateKey = privateKey
 
 def RSA_Generator():
+    print("Generating RSA...")
     p = randprime(a=1, b=LIMIT)
     q = randprime(a=1, b=LIMIT)
+    print("Values chosen:\np = ", p ,"\nq = ", q)
     n = p * q
+    print("n (p*q) = ", n)
     phi = (p - 1) * (q - 1)
+    print("phi (p-1)*(q-1) = ", phi)
 
     d = 0
+    print("Determining 'd'...")
     while True:
-        if d * E % phi == 1:
+        if (d * E) % phi == 1:
             break
         d += 1
+    print("'d' has been chosen. 'd' = ", d)
     
     return KeySet(publicKey=(E, n), privateKey=(d, n))
 
+def hashFunction(plaintextMsg: str):
+    plaintextBytes = str.encode(plaintextMsg)
+    plaintextB64 = base64.b64encode(plaintextBytes, altchars=None)
+    messageHash = hashlib.sha3_256()
+    messageHash.update(plaintextB64)
+    return messageHash.digest()
+
 keyset = RSA_Generator()
 
-plaintext = 5
+originalMessage = "message"
+print("Plaintext: ", originalMessage)
 
-cypher = plaintext**keyset.publicKey[0] % keyset.publicKey[1]
-print(cypher)
+messageHash = hashFunction(originalMessage)
 
-translated = cypher**keyset.privateKey[0] % keyset.privateKey[1]
-print(translated)
+digitalSignature = messageHash**keyset.publicKey[0] % keyset.publicKey[1]
+
+cipherText = originalMessage**keyset.publicKey[0] % keyset.publicKey[1]
+print("Ciphertext: ", cipherText)
+
+translated = cipherText**keyset.privateKey[0] % keyset.privateKey[1]
+print("Decrypted ciphertext: ", translated)
