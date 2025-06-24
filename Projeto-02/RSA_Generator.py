904c29d99246677a11fcbe434cfcab28a1165053
Davi Dias <51387816+DaviDias21@users.noreply.github.com>
2025-06-24 09:14:15 -0300

    Update RSA_Generator.py

--- Projeto-02/RSA_Generator.py
+++ Projeto-02/RSA_Generator.py
@@ -10,7 +10,7 @@ LIMIT = 128
 E = 65537
 
 class ExtendedMessage:
-    def __init__(self, encryptedOriginalMessage:str, digitalSignature:str):
+    def __init__(self, encryptedOriginalMessage:int, digitalSignature:int):
         self.encryptedOriginalMessage = encryptedOriginalMessage
         self.digitalSignature = digitalSignature
 
@@ -19,12 +19,40 @@ class KeySet:
         self.publicKey = publicKey
         self.privateKey = privateKey
 
-def cipherMessage(originalMessage : str, keySet):
-    encryptedMessage = originalMessage**keyset.privateKey[0] % keyset.privateKey[1]
+def cipherMessage(originalMessage : str, keySet : KeySet):
+    print("Encrypting message...")
+    messageInB64 = StringToBase64(originalMessage)
+    encryptedMessage = int.from_bytes(messageInB64)**keyset.privateKey[0] % keyset.privateKey[1]
+    print("Encrypted message: ", encryptedMessage)
     messageHash = hashFunction(originalMessage)
-    digitalSignature = messageHash**keySet.privateKey[0] % keySet.privateKey[1]
+    print("Message hash: ", messageHash)
+    digitalSignature = int.from_bytes(messageHash)**keySet.privateKey[0] % keySet.privateKey[1]
+    print("Encrypted hash (digital signature): ", digitalSignature)
     extMessage = ExtendedMessage(encryptedMessage, digitalSignature)
+    print("Extended message formed.")
     return extMessage
+
+def decipherMessage(extMessage : ExtendedMessage, keySet : KeySet):
+    msgTemp = extMessage.encryptedOriginalMessage**keySet.publicKey[0] % keySet.publicKey[1]
+    decryptedMessageB64 = msgTemp.to_bytes(64)
+    decryptedMessageString = Base64ToString(decryptedMessageB64)
+    msgTemp = extMessage.digitalSignature**keySet.publicKey[0] % keySet.publicKey[1]
+    decryptedHashB64 = msgTemp.to_bytes(64)
+    decryptedHashString = Base64ToString(decryptedHashB64)
+
+    if(HashCheck(decryptedMessageString, decryptedHashString)):
+        print("AUTHENTICITY CHECK: TRUE")
+    else:
+        print("AUTHENTICITY CHECK: FALSE")
+    print("Original message: ", decryptedMessageString)
+    return decryptedMessageString
+    
+def HashCheck(originalMessage : str, messageHash : str):
+    hashCheckAttempt = hashFunction(originalMessage)
+    if(hashCheckAttempt == messageHash):
+        return True
+    else:
+        return False
 
 def RSA_Generator():
     print("Generating RSA...")
@@ -46,23 +74,31 @@ def RSA_Generator():
     
     return KeySet(publicKey=(E, n), privateKey=(d, n))
 
+def StringToBase64(someString : str):
+    stringInBytes = str.encode(someString)
+    print("[StoB64] String in Bytes: ", stringInBytes)
+    stringInB64 = base64.b64encode(stringInBytes, altchars=None)
+    print("[StoB64] String in Base64: ", stringInB64)
+    return stringInB64
+
+def Base64ToString(someB64 : bytes):
+    B64ToBytes = base64.b64decode(someB64, altchars=None)
+    print("[B64BacktoS] Base64 back to Bytes: ", B64ToBytes)
+    B64ToString = B64ToBytes.decode('utf-8')
+    print("[B64BacktoS] Base64 back to String: ", B64ToString)
+    return B64ToString
+
 def hashFunction(plaintextMsg: str):
-    plaintextBytes = str.encode(plaintextMsg)
-    plaintextB64 = base64.b64encode(plaintextBytes, altchars=None)
+    messageInB64 = StringToBase64(plaintextMsg)
     messageHash = hashlib.sha3_256()
-    messageHash.update(plaintextB64)
+    messageHash.update(messageInB64)
     return messageHash.digest()
 
 keyset = RSA_Generator()
 
 originalMessage = "message"
-print("Plaintext: ", originalMessage)
+print("Original message: ", originalMessage)
 
-messageHash = hashFunction(originalMessage)
-digitalSignature = messageHash**keyset.publicKey[0] % keyset.publicKey[1]
+extendedMessage = cipherMessage(originalMessage, keyset)
 
-cipherText = originalMessage**keyset.publicKey[0] % keyset.publicKey[1]
-print("Ciphertext: ", cipherText)
-
-translated = cipherText**keyset.privateKey[0] % keyset.privateKey[1]
-print("Decrypted ciphertext: ", translated)
+decipheredMessage = decipherMessage(extendedMessage, keyset)
