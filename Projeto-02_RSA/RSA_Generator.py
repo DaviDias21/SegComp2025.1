1d2f46a2aedfeffa4ba37fe6b9fae8d455b46884
Davi Dias <davidiasdasilva21@gmail.com>
2025-06-25 02:39:54 -0300

    Quase funcionando (mensagem decifrada vem com nulls na string)
    d é determinnado por funções de MDC Extendido
    Ajustados nomes de funções e de variáveis

--- Projeto-02_RSA/RSA_Generator.py
+++ Projeto-02_RSA/RSA_Generator.py
@@ -1,111 +1,134 @@
 from sympy import randprime
 import hashlib
 import base64
+import random
 
 #LIMIT = 179769313486231590772930519078902473361797697894230657273430081157732675805500963132708477322407536021120113879871393357658789768814416622492847430639474124377767893424865485276302219601246094119453082952085005768838150682342462881473913110540827237163350510684586298239947245938479716304835356329624224137216
 LIMIT = 1024
 E = 65537
 
 class ExtendedMessage:
-    def __init__(self, encryptedOriginalMessage, digitalSignature):
-        self.encryptedOriginalMessage = encryptedOriginalMessage
-        self.digitalSignature = digitalSignature
+    def __init__(self, encryptedMessage_, digitalSignature_):
+        self.encryptedMessage = encryptedMessage_
+        self.digitalSignature = digitalSignature_
 
 class KeySet:
     def __init__(self, publicKey: tuple, privateKey: tuple):
         self.publicKey = publicKey
         self.privateKey = privateKey
 
-def cipherMessage(originalMessage : str, keySet : KeySet):
-    print("Encrypting message...")
-    print("\n[CONVERTING STRING TO BYTES THEN BASE64]")
-    messageInB64 = StringToBase64(originalMessage)
-    print("[STRING CONVERTED TO BASE64]\n")
-    messageIntFromB64 = int.from_bytes(messageInB64)
-    print("Message int from Base64 (before encryption): ", messageIntFromB64)
-    encryptedMessage = (messageIntFromB64)**keyset.privateKey[0] % keyset.privateKey[1]
-    print("Message int after encryption: ", encryptedMessage)
-    messageHash = hashFunction(originalMessage)
-    print("Message hash (bytes): ", messageHash)
-    digitalSignature = (int.from_bytes(messageHash))**keySet.privateKey[0] % keySet.privateKey[1]
-    print("[SENT MESSAGE] Encrypted message (B64 to int): ", encryptedMessage)
-    print("[SENT MESSAGE] Encrypted hash (digital signature): ", digitalSignature)
-    extMessageSent = ExtendedMessage(encryptedMessage, digitalSignature)
-    print("Extended message formed.")
-    return extMessageSent
-
-def decipherMessage(extMessageRecieved : ExtendedMessage, keySet : KeySet):
-    print("[RECIEVED MESSAGE] Encrypted message: ", extMessageRecieved.encryptedOriginalMessage)
-    print("[RECIEVED MESSAGE] Encrypted hash (digital signature): ", extMessageRecieved.digitalSignature)
-    decryptedMessageInt = (extMessageRecieved.encryptedOriginalMessage)**keySet.publicKey[0] % keySet.publicKey[1]
-    print("[RECIEVED MESSAGE] Decrypted message (int from Base64): ", decryptedMessageInt)
-    decryptedMessageB64 = (decryptedMessageInt).to_bytes(8)
-    print("[RECIEVED MESSAGE] Decrypted message (back to Base64): ", decryptedMessageB64)
-    decryptedMessageString = Base64ToString(decryptedMessageB64)
-    msgTemp = (extMessageRecieved.digitalSignature)**keySet.publicKey[0] % keySet.publicKey[1]
-    decryptedHashB64 = (msgTemp).to_bytes(8)
-    decryptedHashString = Base64ToString(decryptedHashB64)
-
-    if(HashCheck(decryptedMessageString, decryptedHashString)):
-        print("AUTHENTICITY CHECK: TRUE")
+def GreatestCommonDivider(a, b):
+    """
+    Performs the Euclidean algorithm and returns the gcd of a and b
+    """
+    if (b == 0):
+        return a
     else:
-        print("AUTHENTICITY CHECK: FALSE")
-    print("Original message: ", decryptedMessageString)
-    return decryptedMessageString
-    
-def HashCheck(originalMessage : str, messageHash : str):
-    hashCheckAttempt = hashFunction(originalMessage)
+        return GreatestCommonDivider(b, a % b)
+
+def ExtendedGCD(a, b):
+    """
+    Performs the extended Euclidean algorithm
+    Returns the gcd, coefficient of a, and coefficient of b
+    """
+    x, old_x = 0, 1
+    y, old_y = 1, 0
+
+    while (b != 0):
+        quotient = a // b
+        a, b = b, a - quotient * b
+        old_x, x = x, old_x - quotient * x
+        old_y, y = y, old_y - quotient * y
+
+    return a, old_x, old_y
+        
+def GenerateKeySet():
+    print("Generating Key Set...")
+    primeNo1 = randprime(1, LIMIT)
+    primeNo2 = randprime(1, LIMIT)
+    print("Values chosen:\np = ", primeNo1 ,"\nq = ", primeNo2)
+    primesProduct = primeNo1 * primeNo2
+    print("n (p*q) = ", primesProduct)
+    totient = (primeNo1 - 1) * (primeNo2 - 1)
+    print("totient (p-1)*(q-1) = ", totient)
+    GreatestCommonDivider, tempD, y = ExtendedGCD(E, totient)
+    if (tempD < 0):
+        d = tempD + totient
+    else:
+        d = tempD
+
+    print("'d' has been chosen: d = ", d)
+    return KeySet(publicKey=(E, primesProduct), privateKey=(d, primesProduct))
+
+def MessageBytesToInt(someMessage : bytes):
+    messageBytesToInt = int.from_bytes(someMessage)
+    return messageBytesToInt
+
+def MessageBytesToString(someMessage : bytes):
+    messageBackToString = someMessage.decode()
+    return messageBackToString
+
+def GetMessageHash(plaintextMsg: str):
+    messageInBytes = plaintextMsg.encode()
+    messageHash = hashlib.sha3_256()
+    messageHash.update(messageInBytes)
+    return messageHash.digest()
+
+def HashComparison(messageString : str, messageHash : bytes):
+    hashCheckAttempt = GetMessageHash(messageString)
+    print("Decrypted message: ", messageString, "\n\nHash Attempt: ", hashCheckAttempt, "\nOriginal Hash: ", messageHash)
     if(hashCheckAttempt == messageHash):
         return True
     else:
         return False
+    
+def cipherMessage(originalMessage : str, keySet : KeySet):
+    print("Encrypting message...")
+    messageInt = MessageBytesToInt(originalMessage.encode())
+    print("\nMessage int: ", messageInt)
+    encryptedInt = ((messageInt)**keySet.privateKey[0]) % keySet.privateKey[1]
+    print("Encrypted message int: ", encryptedInt)
+    print("\nParsing to Base64...")
+    encryptedMessageB64 = base64.b64encode((encryptedInt).to_bytes(8))
+    print("\nCalculating digital signature...")
+    messageHash = GetMessageHash(originalMessage)
+    print("\nMessage hash: ", messageHash)
+    digitalSignatureInt = ((int.from_bytes(messageHash))**keySet.privateKey[0]) % keySet.privateKey[1]
+    print("DigSign int: ", digitalSignatureInt)
+    digitalSignatureB64 = base64.b64encode((digitalSignatureInt).to_bytes(16, "big"))
+    print("DigSign B64: ", digitalSignatureB64)
+    extMessageSent = ExtendedMessage(encryptedMessageB64, digitalSignatureB64)
+    print("\n[SENT MESSAGE] Encrypted message (Base64): ", encryptedMessageB64)
+    print("[SENT MESSAGE] Digital signature (Base64): ", digitalSignatureB64)
+    return extMessageSent 
 
-def RSA_Generator():
-    print("Generating RSA...")
-    p = randprime(a=1, b=LIMIT)
-    q = randprime(a=1, b=LIMIT)
-    print("Values chosen:\np = ", p ,"\nq = ", q)
-    n = p * q
-    print("n (p*q) = ", n)
-    phi = (p - 1) * (q - 1)
-    print("phi (p-1)*(q-1) = ", phi)
 
-    d = 0
-    print("Determining 'd'...")
-    while True:
-        if (d * E) % phi == 1:
-            break
-        d += 1
-    print("'d' has been chosen. 'd' = ", d)
-    return KeySet(publicKey=(E, n), privateKey=(d, n))
+def decipherMessage(extMessageRecieved : ExtendedMessage, keySet : KeySet):
+    # Convert both message and signature from Base64 to bytes
+    print("\n[RECIEVED MESSAGE] Encrypted message (Base64): ", extMessageRecieved.encryptedMessage)
+    print("[RECIEVED MESSAGE] Digital signature (Base64): ", extMessageRecieved.digitalSignature)
 
-def StringToBase64(someString : str):
-    stringInBytes = someString.encode()
-    print("[StoB64] String in Bytes: ", stringInBytes)
-    stringInB64 = base64.b64encode(stringInBytes, altchars=None)
-    print("[StoB64] String in Base64: ", stringInB64)
-    return stringInB64
+    print("\nConverting from Base64 and decrypting...")
+    decryptedMessageInt = ((int.from_bytes(base64.b64decode(extMessageRecieved.encryptedMessage)))**keySet.publicKey[0]) % keySet.publicKey[1]
+    decryptedSignatureInt = (int.from_bytes(base64.b64decode(extMessageRecieved.digitalSignature)))
+    print("DigSign int: ", decryptedSignatureInt)
+    decryptedMessage = MessageBytesToString((decryptedMessageInt).to_bytes(8, "big"))
+    cleanedDecryptedMessage = decryptedMessage.rstrip('\x00')
+    decryptedSignature = (decryptedSignatureInt).to_bytes(16, "big")
 
-def Base64ToString(someB64 : bytes):
-    print("SomeB64: ", someB64)
-    B64ToBytes = base64.b64decode(someB64)
-    print("[B64BacktoS] Base64 back to Bytes: ", B64ToBytes)
-    B64ToString = B64ToBytes.decode()
-    print("[B64BacktoS] Base64 back to String: ", B64ToString)
-    return B64ToString
+    if(HashComparison(cleanedDecryptedMessage, decryptedSignature)):
+        print("AUTHENTICITY CHECK: TRUE")
+    else:
+        print("AUTHENTICITY CHECK: FALSE")
+    return decryptedMessage
 
-def hashFunction(plaintextMsg: str):
-    print("[INSIDE HASHFUNCTION]")
-    messageInB64 = StringToBase64(plaintextMsg)
-    messageHash = hashlib.sha3_256()
-    messageHash.update(messageInB64)
-    print("[HASHFUNCTION RETURNED]")
-    return messageHash.digest()
+usedKeyset = GenerateKeySet()
 
-keyset = RSA_Generator()
-
-originalMessage = "a"
+originalMessage = "me"
 print("Original message: ", originalMessage)
 
-extendedMessage = cipherMessage(originalMessage, keyset)
-decipheredMessage = decipherMessage(extendedMessage, keyset)
+extendedMessage = cipherMessage(originalMessage, usedKeyset)
+decipheredMessage = decipherMessage(extendedMessage, usedKeyset)
+print("Original message: ", decipheredMessage)
+
+input()
